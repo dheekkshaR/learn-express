ce2d0c4c6a197d7eede5856c4699cdea56afec4b
dheekkshaR <rajeshbabu.d@northeastern.edu>
2024-03-12 16:08:25 -0400

    monolith to seperate

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,26 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+  if (req.users) {
+    let usernames = req.users.map(user => ({ id: user.id, username: user.username }));
+    res.send(usernames);
+  } else {
+    res.status(404).json({
+      error: { message: 'Users not found', status: 404 }
+    });
+  }
+});
+
+router.get('/username/:name', (req, res) => {
+    const username = req.params.name; // Get the username from request parameters
+    const user = req.users.find(user => user.username === username); // Find user by username
+    if (user) {
+      req.userEmail = user.email; // Add user's email to request object
+      res.status(200).json([{ email: user.email }]);
+    } else {
+      res.status(404).json({ error: { message: 'User not found', status: 404 } });
+    }
+  });
+
+module.exports = router;
--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -1,4 +1,26 @@
-const express = require('express')
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const router = express.Router()
+const router = express.Router();
+
+router.post('/adduser', (req, res) => {
+  if (!req.users) {
+    return res.status(404).json({
+      error: { message: 'Users not found', status: 404 }
+    });
+  }
+
+  let newuser = req.body;
+  req.users.push(newuser);
+  fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users), (err) => {
+    if (err) {
+      console.log('Failed to write');
+      res.status(500).send('Failed to write');
+    } else {
+      console.log('User Saved');
+      res.send('done');
+    }
+  });
+});
+
+module.exports = router;
